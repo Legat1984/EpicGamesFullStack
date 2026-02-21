43c5be70e401583fea9142043b033c9e568da158
Rudnik Sergey (Legat) <indeo_1984@mail.ru>
2026-02-22 02:36:22 +0300

    Немного изменили структуру проекта на сервере

--- server/routes/games.js
+++ server/routes/games.js
@@ -7,7 +7,6 @@ const express = require('express')
 // Модели базы данных
 const User = require('../models/User')
 const Games = require('../models/Games')
-const Parts = require('../models/HPTheBattleOfHogwarts/Parts')
 
 // Middleware
 const authMiddleware = require('../middleware/authMiddleware');
